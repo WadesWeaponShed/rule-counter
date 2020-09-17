7446cd1d09e3498558a319a805a4d3b03d7c9528
Adam Forester <aforester@silverdogstudios.com>
2020-09-17 13:58:13 -0500

    v2

--- rule_counter.sh
+++ rule_counter.sh
@@ -8,13 +8,13 @@ printf "\nWhat is the Policy Package Name?\n"
 read POL_NAME
 POL2=$(echo $POL_NAME | tr -d ' ')
 
-mgmt_cli -r true show access-rulebase name "$POL_NAME" details-level full limit 500 --format json use-object-dictionary true |jq '.total' >total.txt
+mgmt_cli -r true -d $DOMAIN show access-rulebase name "$POL_NAME" details-level full limit 500 --format json use-object-dictionary true |jq '.total' >total.txt
 
-mgmt_cli -r true show access-rulebase name "$POL_NAME" details-level full limit 500 --format json use-object-dictionary true |jq '.rulebase[]| select(."inline-layer" == null|not) | ."inline-layer"' >inlinelayers.txt
+mgmt_cli -r true -d $DOMAIN show access-rulebase name "$POL_NAME" details-level full limit 500 --format json use-object-dictionary true |jq '.rulebase[]| select(."inline-layer" == null|not) | ."inline-layer"' >inlinelayers.txt
 
 for line in $(cat inlinelayers.txt) ;
 do
-    mgmt_cli -r true show access-rulebase uid $line details-level full limit 500 --format json use-object-dictionary true |jq '.total' >>total.txt
+    mgmt_cli -r true -d $DOMAIN show access-rulebase uid $line details-level full limit 500 --format json use-object-dictionary true |jq '.total' >>total.txt
 done
 
 printf "\nTotal Number of Rules includeing Layers\n"
